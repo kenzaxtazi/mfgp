16fa047fb125836af014b4e2a295702f8f5f1c4f
KenzaxTazi <ktazi100@yahoo.co.uk>
2022-04-30 14:46:40 +0100

    dims no longer a function arg for models

--- models.py
+++ models.py
@@ -6,8 +6,9 @@ from emukit.multi_fidelity.models import GPyLinearMultiFidelityModel
 from emukit.multi_fidelity.models.non_linear_multi_fidelity_model import make_non_linear_kernels, NonLinearMultiFidelityModel
 
 
-def linear_mfdgp(X_train, Y_train, dims=1, fidelities=2):
+def linear_mfdgp(X_train, Y_train, fidelities=2):
     """ Create a linear MFDGP model """
+    dims = X_train.shape[1]
     base_kernels = [GPy.kern.RBF(dims), GPy.kern.RBF(dims)]
     kernel = emukit.multi_fidelity.kernels.LinearMultiFidelityKernel(base_kernels)
     unwrp_model = GPyLinearMultiFidelityModel(X_train, Y_train, kernel, n_fidelities= fidelities)
@@ -17,13 +18,17 @@ def linear_mfdgp(X_train, Y_train, dims=1, fidelities=2):
     return model
 
 
-def nonlinear_mfdgp(X_train, Y_train, dims=1, fidelities=2):
+def nonlinear_mfdgp(X_train, Y_train, fidelities=2):
     """ Create a nonlinear MFDGP model """
+    dims = X_train.shape[1]
     base_kernel = GPy.kern.RBF
-    kernels = make_non_linear_kernels(base_kernel, fidelities, X_train.shape[1] - 1)
+    kernels = make_non_linear_kernels(base_kernel, fidelities, dims - 1)
     model = NonLinearMultiFidelityModel(X_train, Y_train, n_fidelities=fidelities, kernels=kernels, verbose=True, optimization_restarts=5)
     m1, m2 = model.models
     m1.Gaussian_noise.variance.fix(0)
     m2.Gaussian_noise.variance.fix(0)
     return model
+
+def save(model, name):
+    """TO DO"""
     
