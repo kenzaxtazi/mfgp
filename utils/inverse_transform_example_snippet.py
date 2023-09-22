6841033c3d1af8c817f24e517b7ef5dad7d068d6
Kenza Tazi <ktazi100@yahoo.co.uk>
2023-09-22 16:23:15 +0100

    moved to utils

--- utils/inverse_transform_example_snippet.py
+++ utils/inverse_transform_example_snippet.py
@@ -3,10 +3,10 @@ import numpy as np
 import scipy as sp
 
 # Read in the data
-df = pd.read_csv('mfgp_predictions_1980_2010.csv', index_col=0)
+df = pd.read_csv('mfgp_predictions_1980_2013.csv', index_col=0)
 
 # Read in the scaling factors
-scaling_df = pd.read_csv('lambdas_1980_2010.csv', index_col=0)
+scaling_df = pd.read_csv('lambdas_1980_2013.csv', index_col=0)
 
 # For 1980
 df_subset = df.set_index('time')['1980-01-01':'1981-01-01']
