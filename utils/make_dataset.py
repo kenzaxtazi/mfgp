f3a60d66a7e1a8e701d126890c50aaa3cf88d8bd
Kenza Tazi <ktazi100@yahoo.co.uk>
2023-09-22 16:21:42 +0100

    condense

--- utils/make_dataset.py
+++ utils/make_dataset.py
@@ -8,10 +8,11 @@ from scipy import special
 
 
 directory = 'experiments/exp3/outputs_all/*'
+tr_directory = 'experiments/exp3/tr_outputs_all/'
 paths = glob.glob(directory)
 
-# Info for inverse transform           
-scaling_df = pd.read_csv('experiments/exp3/lambdas_1980_2013_.csv')
+# Info for inverse transform
+scaling_df = pd.read_csv('experiments/exp3/lambdas_1980_2013.csv')
 lambdas = scaling_df[['lambdas']]
 
 i = 0
@@ -19,28 +20,28 @@ i = 0
 for path in paths:
     print(path)
     df = pd.read_csv(path, index_col=0)
-    
+
     # Transformed mean
     df['pred_tr'] = special.inv_boxcox(df['pred0'], lambdas.iloc[i].values)
-    i =+ 1
-    
+    i = + 1
+
     # Transformed upper confidence interval
     pred_CI_upper = df['pred0'] + 1.96 * np.sqrt(df['y_var0'])
-    df['pred_tr_CI_upper'] = special.inv_boxcox(pred_CI_upper, lambdas.iloc[0].values)
-    
+    df['pred_tr_CI_upper'] = special.inv_boxcox(
+        pred_CI_upper, lambdas.iloc[0].values)
+
     # Transformed lower confidence interval
     pred_CI_lower = df['pred0'] - 1.96 * np.sqrt(df['y_var0'])
-    df['pred_tr_CI_lower'] = special.inv_boxcox(pred_CI_lower, lambdas.iloc[0].values)
-    
+    df['pred_tr_CI_lower'] = special.inv_boxcox(
+        pred_CI_lower, lambdas.iloc[0].values)
+
     p = path.split('/')
-    df.to_csv('experiments/exp3/tr_outputs_all/'+p[-1])
+    df.to_csv(tr_directory + p[-1])
 
-
-tr_directory = 'experiments/exp3/tr_outputs_all/'
 tr_paths = glob.glob(tr_directory)
 tr_df_all = pd.concat(map(pd.read_csv, tr_paths))
 tr_df_all.drop(columns=['Unnamed: 0'], inplace=True)
-tr_df_all.rename(columns={'elevation':'elev'}, inplace=True)
+tr_df_all.rename(columns={'elevation': 'elev'}, inplace=True)
 tr_df_all['date'] = pd.to_datetime(tr_df_all['time']).astype("datetime64[M]")
 tr_df_all.drop(columns='time', inplace=True)
 tr_df_all.to_csv('experiments/exp3/mfgp_predictions_1980_2013.csv')
